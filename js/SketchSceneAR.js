595d91933c653f09412a58f85023d6eebd23a14e
Kathy Son <kson1128@gmail.com>
2021-05-06 17:51:39 -0400

    added start,stop, purple button and functionality

--- js/SketchSceneAR.js
+++ js/SketchSceneAR.js
@@ -2,7 +2,15 @@
 
 import React, { Component } from "react"
 
-import { StyleSheet } from "react-native"
+import {
+  StyleSheet,
+  AppRegistry,
+  Text,
+  View,
+  PixelRatio,
+  TouchableHighlight,
+  Button
+} from "react-native"
 
 import {
   ViroARScene,
@@ -11,9 +19,11 @@ import {
   ViroFlexView,
   ViroImage,
   ViroPolyline,
-  ViroMaterials
+  ViroMaterials,
+  ViroButton
 } from "react-viro"
 
+//add texture
 ViroMaterials.createMaterials({
   red: { diffuseColor: "red" },
   blue: { diffuseColor: "blue" },
@@ -32,26 +42,58 @@ export default class SketchSceneAR extends Component {
     // Set initial state here
     this.state = {
       thickness: 0.1,
-      points: [[0, 0, 0]]
+      points: [[0, 0, 0]],
+      drawing: false,
+      polyline: [],
+      color: "white"
     }
 
     // bind 'this' to functions
     this._onCameraARHitTest = this._onCameraARHitTest.bind(this)
     this._onInitialized = this._onInitialized.bind(this)
+    this._toggleDraw = this._toggleDraw.bind(this)
+    this._toggleColor = this._toggleColor.bind(this)
   }
 
   render() {
     return (
       <ViroARScene
-        onTrackingUpdated={this._onInitialized}
         onCameraARHitTest={this._onCameraARHitTest}
+        onTrackingUpdated={this._onInitialized}
       >
+        <ViroButton
+          source={
+            !this.state.drawing
+              ? require("./res/startdrawing.png")
+              : require("./res/stopdrawing.png")
+          }
+          // gazeSource={require("./res/startdrawing.png")}
+          // tapSource={require("./res/stopdrawing.png")}
+          position={[1, 3, -5]}
+          height={1}
+          width={1}
+          onClick={this._toggleDraw}
+          onGaze={this._onButtonGaze}
+        />
+
+        <ViroButton
+          source={require("./res/purple.png")}
+          // gazeSource={require("./res/purple.png")}
+          // tapSource={require("./res/purple.png")}
+          position={[2, 3, -6]}
+          height={1}
+          width={1}
+          onClick={() => this._toggleColor("purple")}
+          onGaze={this._onButtonGaze}
+        />
+
         <ViroPolyline
           position={[0, 0, -2]}
           points={this.state.points}
           thickness={this.state.thickness}
-          materials={"white"}
+          materials={this.state.color}
         />
+
         {/*<ViroPolyline
           position={[-1, -1, -0.9]}
           points={[
@@ -76,18 +118,35 @@ export default class SketchSceneAR extends Component {
     )
   }
 
+  _toggleDraw() {
+    const current = this.state.drawing
+    this.setState({
+      drawing: !current
+    })
+  }
+
+  _toggleColor(colorName) {
+    // console.log("color", colorName)
+    this.setState({
+      color: colorName
+    })
+  }
+
   _onCameraARHitTest(results) {
-    if (results.hitTestResults.length > 0) {
-      for (var i = 0; i < results.hitTestResults.length; i++) {
-        let result = results.hitTestResults[i]
-        if (
-          result.type == "ExistingPlaneUsingExtent" ||
-          result.type == "FeaturePoint"
-        ) {
-          this.setState({
-            points: [...this.state.points, result.transform.position]
-          })
-          break
+    if (this.state.drawing) {
+      if (results.hitTestResults.length > 0) {
+        for (var i = 0; i < results.hitTestResults.length; i++) {
+          let result = results.hitTestResults[i]
+          if (
+            result.type == "ExistingPlaneUsingExtent" ||
+            result.type == "FeaturePoint" ||
+            result.type == "ExistingPlane"
+          ) {
+            this.setState({
+              points: [...this.state.points, result.transform.position]
+            })
+            break
+          }
         }
       }
     }
@@ -111,6 +170,61 @@ var styles = StyleSheet.create({
     color: "#ffffff",
     textAlignVertical: "center",
     textAlign: "center"
+  }
+})
+
+var localStyles = StyleSheet.create({
+  viroContainer: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  outer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  inner: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  titleText: {
+    paddingTop: 30,
+    paddingBottom: 20,
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 25
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 20
+  },
+  buttons: {
+    height: 80,
+    width: 150,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#68a0cf",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
+  },
+  exitButton: {
+    height: 50,
+    width: 100,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#68a0cf",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
   }
 })
 
