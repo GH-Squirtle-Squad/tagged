4a8777564431cfdf8a5780f0c12bd98e6b7d23d7
Kathy Son <kson1128@gmail.com>
2021-05-06 11:14:52 -0400

    added viroimage and viroflexview

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,42 +1,61 @@
-'use strict';
+"use strict"
 
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native"
 
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
-} from 'react-viro';
+  ViroImage,
+  ViroFlexView
+} from "react-viro"
 
 export default class HelloWorldSceneAR extends Component {
-
   constructor() {
-    super();
+    super()
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
-    };
+      text: "Initializing AR..."
+    }
 
     // bind 'this' to functions
-    this._onInitialized = this._onInitialized.bind(this);
+    this._onInitialized = this._onInitialized.bind(this)
   }
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+
+        <ViroFlexView
+          style={{ flexDirection: "row", padding: 0.1 }}
+          width={5.0}
+          height={5.0}
+          position={[-5.0, 0.0, -2.0]}
+          rotation={[0, 45, 0]}
+        >
+          <ViroImage
+            source={require("./res/guadalupe_360.jpg")}
+            style={{ flex: 0.5 }}
+          />
+        </ViroFlexView>
       </ViroARScene>
-    );
+    )
   }
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
-      });
+        text: "Hello World!"
+      })
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
@@ -45,12 +64,12 @@ export default class HelloWorldSceneAR extends Component {
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
-});
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center"
+  }
+})
 
-module.exports = HelloWorldSceneAR;
+module.exports = HelloWorldSceneAR
