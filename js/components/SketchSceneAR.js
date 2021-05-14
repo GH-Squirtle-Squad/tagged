d0fdbff67114037d2e051958e3e8e5329b320074
Kelsey Siman <ksiman14@gmail.com>
2021-05-14 09:32:45 -0400

    refactor: removed unused coord for points

--- js/components/SketchSceneAR.js
+++ js/components/SketchSceneAR.js
@@ -31,7 +31,7 @@ export default class SketchSceneAR extends Component {
     // Set initial state here
     this.state = {
       thickness: 0.1,
-      points: [[0, 0, -3]],
+      points: [[0, 0]],
       polylines: [],
       writeAccessPermission: false,
       readAccessPermission: false,
@@ -53,7 +53,7 @@ export default class SketchSceneAR extends Component {
     if (color !== oldColor && drawing) {
       this.setState({
         polylines: [...polylines, { points: points, color: oldColor }],
-        points: [[0, 0, -3]],
+        points: [[0, 0]],
         color: color
       })
     }
@@ -61,7 +61,7 @@ export default class SketchSceneAR extends Component {
     if (!drawing && polylines.length > 0) {
       this.setState({
         polylines: [],
-        points: [[0, 0, -3]]
+        points: [[0, 0]]
       })
     }
   }
@@ -101,7 +101,7 @@ export default class SketchSceneAR extends Component {
   _reset() {
     this.setState({
       polylines: [],
-      points: [[0, 0, -3]]
+      points: [[0, 0]]
     })
   }
 
